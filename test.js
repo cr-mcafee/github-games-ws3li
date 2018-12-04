ac94060310c21a978f009612d19081075c9bf6e7
ws3li <wilsonli2009.work@gmail.com>
2018-12-04 15:02:06 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 const hello = () => {
-  // @todo Make console into an Alert message
+  /**
+ * @todo Make console log into Alert
+ * @body Pop up message instead of console log
+ */
   console.log("hello world");
 }
 
